7d36eb7fdc68bc02c53be5a73dd03da5a9559b0a
Kesavan Sriramulu <kesavan.sriram@zohomail.in>
2026-01-16 18:01:52 +0530

    For base setup

--- SERVICE.LAB/SERVICE.LIS/Program.cs
+++ SERVICE.LAB/SERVICE.LIS/Program.cs
@@ -85,7 +85,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "LIMS SERVICE", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "LIS SERVICE", Version = "v1" });
 
     c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
     {
@@ -281,7 +281,7 @@ app.UseSwaggerUI(c =>
         ? "/swagger/v1/swagger.json"
         : $"{basePath}/swagger/v1/swagger.json";
 
-    c.SwaggerEndpoint(swaggerBasePath, "LIMS SERVICE");
+    c.SwaggerEndpoint(swaggerBasePath, "LIS SERVICE");
     c.RoutePrefix = "swagger";
 });
 
